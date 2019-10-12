ac6978a5890d57c7693bbbdc5f15078e03357466
aldisuryap <appleimport123@gmail.com>
2019-10-12 17:00:47 +0700

    test

---
+++ app/src/main/java/polinema/ac/id/androiduistarter/fragments/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package polinema.ac.id.androiduistarter.fragments.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import polinema.ac.id.androiduistarter.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
